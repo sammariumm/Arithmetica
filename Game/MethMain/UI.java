9d30ccbbbe8909c7643e4256c67e472bca085330
Sam Manzano <sam.mnzn0@gmail.com>
2025-11-11 13:59:35 +0800

    minor ui changes (display math prompt)

--- Game/MethMain/UI.java
+++ Game/MethMain/UI.java
@@ -1,7 +1,9 @@
 package Game.MethMain;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.util.Random;
 
 import Game.Object.OBJ_Heart;
 import Game.MethMain.MethGamePanel;
@@ -16,6 +18,7 @@ public class UI
     Graphics2D g2;
     Font font;
     BufferedImage heart_full, heart_half, heart_empty;
+    String[] mathPrompt = new String[10];
     
     public UI(MethGamePanel gp)
     {
@@ -38,6 +41,8 @@ public class UI
 
         // game state
         drawPlayerLife();
+        String mp = setMathPrompt();
+        drawMathPrompt(mp);
     }
 
     public void drawPlayerLife()
@@ -72,4 +77,68 @@ public class UI
             x += gp.tileSize;
         }
     }
+
+    public void drawMathPrompt(String mp)
+    {
+        // window
+        int x = gp.tileSize * 2;
+        int y = gp.tileSize * 2;
+        int width = gp.screenWidth - (gp.tileSize * 4);
+        int height = gp.tileSize * 2;
+
+        drawSubWindow(x, y, width, height);
+
+        x += gp.tileSize;
+        y += gp.tileSize;
+
+        g2.drawString(mp, x, y);
+    }
+
+    public void drawSubWindow(int x, int y, int width, int height)
+    {
+        Color c = new Color(0,0,0, 150);
+        g2.setColor(c);
+        g2.fillRoundRect(x, y, width, height, 35, 35);
+
+        c = new Color(255,255,255);
+        g2.setColor(c);
+        g2.setStroke(new BasicStroke(4));
+        g2.drawRoundRect(x+5, y+5, width-10, height-10, 25, 25);
+    }
+
+    public String setMathPrompt()
+    {
+        String returnString = "";
+        Random random = new Random();
+        int index = random.nextInt(4);
+        boolean correctEnemySlain = false;
+
+        mathPrompt[0] = "1 + 1";
+        mathPrompt[1] = "2 + 2";
+        mathPrompt[2] = "given an integral";
+        mathPrompt[3] = "burat k ba";
+        mathPrompt[4] = "hihi";
+
+        switch(index)
+        {
+            case 1:
+                returnString = mathPrompt[0];
+                break;
+            case 2:
+                returnString = mathPrompt[1];
+                break;
+            case 3:
+                returnString = mathPrompt[2];
+                break;
+            case 4:
+                returnString = mathPrompt[3];
+                break;
+            case 5:
+                returnString = mathPrompt[4];
+                break;
+        }
+
+        return returnString;
+    }
+
 }
